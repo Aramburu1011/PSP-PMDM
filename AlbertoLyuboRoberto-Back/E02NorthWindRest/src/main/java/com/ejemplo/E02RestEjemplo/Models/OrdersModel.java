5caabaa4014c20d8b40752b478a49783cda831b3
roberss11 <73604636+roberss11@users.noreply.github.com>
2021-05-29 18:10:28 +0200

    Add files via upload

---
+++ AlbertoLyuboRoberto-Back/E02NorthWindRest/src/main/java/com/ejemplo/E02RestEjemplo/Models/OrdersModel.java
@@ -0,0 +1,270 @@
+package com.ejemplo.E02RestEjemplo.Models;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.sql.DataSource;
+
+import com.ejemplo.E02RestEjemplo.DBFactory.DBFactory;
+import com.ejemplo.E02RestEjemplo.Entities.Order;
+
+
+
+public class OrdersModel {
+    
+    Connection conexion = null;
+
+    public OrdersModel() throws SQLException {
+	DataSource ds = DBFactory.getMySQLDataSource();
+	conexion = ds.getConnection();
+    }
+
+    public Order read(Integer id) {
+	Order pedido = null;
+	Statement sentencia = null;
+
+	String sql = "SELECT `id`, `employee_id`, `customer_id`, `order_date`, "
+		+ "`shipped_date`, `shipper_id`, `ship_name` , `ship_address`,"
+		+ "`ship_city`, `ship_state_province`, `ship_zip_postal_code`, "
+		+ "`ship_country_region`, `shipping_fee`, "
+		+ "`taxes`, `payment_type`, `paid_date`, "
+		+ "`notes`, `tax_rate`, `tax_state_id`, `status_id` " + "FROM orders "
+		+ "WHERE id = " + id;
+
+	try {
+	    sentencia = conexion.createStatement();
+	    ResultSet rs = sentencia.executeQuery(sql);
+	    while (rs.next()) { // Si hay un pedido que existe
+		pedido = new Order(
+			rs.getInt("id"),
+			rs.getString("employee_id"),
+			rs.getString("customer_id"),
+			rs.getString("order_date"),
+			rs.getString("shipped_date"),
+			rs.getString("shipper_id"),
+			rs.getString("ship_name"),
+			rs.getString("ship_address"),
+			rs.getString("ship_city"),
+			rs.getString("ship_state_province"),
+			rs.getString("ship_zip_postal_code"),
+			rs.getString("ship_country_region"),
+			rs.getString("shipping_fee"),
+			rs.getString("taxes"),
+			rs.getString("payment_type"),
+			rs.getString("paid_date"),
+			rs.getString("notes"),
+			rs.getString("taxes_rate"),
+			rs.getString("tax_state_id"),
+			rs.getString("status_id"));
+	    };
+	    
+	} catch (SQLException e) {
+	    System.err.println("Error en read de Pedidos: " + e.getMessage());
+	    return null;
+	}
+
+	return pedido;
+    }
+
+    /**
+     * 
+     * @param pedido
+     * @return Devuelve el id del registro recien insertado
+     */
+    public Integer insert(Order pedido) throws  SQLException {
+	Integer id = null;
+	PreparedStatement ps = null;
+	String sql = "INSERT INTO orders ( "
+		+ "`employee_id`, `customer_id`, `order_date`, "
+		+ "`shipped_date`, `shipper_id`, `ship_name` , `ship_address`,"
+		+ "`ship_city`, `ship_state_province`, `ship_zip_postal_code`, `ship_country_region`, `shipping_fee`, "
+		+ "`taxes`, `payment_type`, `paid_date`, `notes`, `tax_rate`, `tax_state_id`, `status_id`) "
+		+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+	try {
+	    ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+	    ps.setString(1, pedido.getEmployee_id());
+	    ps.setString(2, pedido.getCustomer_id());
+	    ps.setString(3, pedido.getOrder_date());
+	    ps.setString(4, pedido.getShipped_date());
+	    ps.setString(5, pedido.getShipper_id());
+	    ps.setString(6, pedido.getShip_name());
+	    ps.setString(7, pedido.getShip_address());
+	    ps.setString(8, pedido.getShip_city());
+	    ps.setString(9, pedido.getShip_state_province());
+	    ps.setString(10, pedido.getShip_zip_postal_code());
+	    ps.setString(11, pedido.getShip_country_region());
+	    ps.setString(12, pedido.getShipping_fee());
+	    ps.setString(13, pedido.getTaxes());
+	    ps.setString(14, pedido.getPayment_type());
+	    ps.setString(15, pedido.getPaid_date());
+	    ps.setString(16, pedido.getNotes());
+	    ps.setString(17, pedido.getTax_rate());
+	    ps.setString(18, pedido.getTax_status_id());
+	    ps.setString(19, pedido.getStatus_id());
+	    if (ps.executeUpdate() > 0) {
+		ResultSet rs = ps.getGeneratedKeys();
+		if (rs.next()) {
+		    id = rs.getInt(1);
+		}
+	    }
+
+	} catch (SQLException e) {
+	    System.err.println("Error al insertar Orders: " + e.getMessage());
+	    throw e;
+	}
+
+	return id;
+    }
+
+    public Boolean delete(Integer idpedidos) throws SQLException {
+	Boolean resultado = false;
+
+	PreparedStatement ps = null;
+	String sql = "DELETE FROM orders where id = ?";
+	try {
+	    ps = conexion.prepareStatement(sql);
+
+	    ps.setInt(1, idpedidos);
+
+	    resultado = (ps.executeUpdate() > 0);
+
+	} catch (SQLException e) {
+	    System.err.println("Error al borrar Pedido: " + e.getMessage());
+	    throw e;
+	}
+
+	return resultado;
+    }
+
+    public Boolean update(Order pedido) throws SQLException  {
+	Boolean resultado = false;
+
+	PreparedStatement ps = null;
+	String sql = "UPDATE orders set "
+		+ "employee_id = ?, "
+		+ "customer_id = ?, "
+		+ "order_date = ?, "
+		+ "shipped_date = ?, "
+		+ "shipper_id = ?, "
+		+ "ship_name  = ?, "
+		+ "ship_address = ?, "
+		+ "ship_city = ?, "
+		+ "ship_state_province = ?, "
+		+ "ship_zip_postal_code = ?, "
+		+ "ship_country_region = ?, "
+		+ "shipping_fee = ?, "
+		+ "taxes = ?, "
+		+ "payment_type = ?, "
+		+ "paid_date = ?, "
+		+ "notes = ?, "
+		+ "tax_rate = ?, "
+		+ "tax_status_id = ?, "
+		+ "status_id = ? "
+		+ "where id = ?";
+	try {
+	    ps = conexion.prepareStatement(sql);
+	    ps.setString(1, pedido.getEmployee_id());
+	    ps.setString(2, pedido.getCustomer_id());
+	    ps.setString(3, pedido.getOrder_date());
+	    ps.setString(4, pedido.getShipped_date());
+	    ps.setString(5, pedido.getShipper_id());
+	    ps.setString(6, pedido.getShip_name());
+	    ps.setString(7, pedido.getShip_address());
+	    ps.setString(8, pedido.getShip_city());
+	    ps.setString(9, pedido.getShip_state_province());
+	    ps.setString(10, pedido.getShip_zip_postal_code());
+	    ps.setString(11, pedido.getShip_country_region());
+	    ps.setString(12, pedido.getShipping_fee());
+	    ps.setString(13, pedido.getTaxes());
+	    ps.setString(14, pedido.getPayment_type());
+	    ps.setString(15, pedido.getPaid_date());
+	    ps.setString(16, pedido.getNotes());
+	    ps.setString(17, pedido.getTax_rate());
+	    ps.setString(18, pedido.getTax_status_id());
+	    ps.setString(19, pedido.getStatus_id());
+	    ps.setInt(20, pedido.getId());
+
+	    resultado = (ps.executeUpdate() > 0);
+
+	} catch (SQLException e) {
+	    System.err.println("Error al actualizar Pedido: " + e.getMessage());
+	    throw e;
+	}
+
+	return resultado;
+    }
+
+    public ArrayList<Order> lista(String filtro, Integer limite, Integer offset)
+
+    {
+	ArrayList<Order> pedidos = new ArrayList<Order>();
+	Statement sentencia = null;
+
+	String sql = "SELECT `id`, "
+		+ "`employee_id`, "
+		+ "`customer_id`, "
+		+ "`order_date`, "
+		+ "`shipped_date`, "
+		+ "`shipper_id`, "
+		+ "`ship_name` , "
+		+ "`ship_address`,"
+		+ "`ship_city`, "
+		+ "`ship_state_province`, "
+		+ "`ship_zip_postal_code`, "
+		+ "`ship_country_region`, "
+		+ "`shipping_fee`, "
+		+ "`taxes`, "
+		+ "`payment_type`, "
+		+ "`paid_date`, "
+		+ "`notes`, "
+		+ "`tax_rate`, "
+		+ "`tax_status_id`, "
+		+ "`status_id` "
+		+ "FROM orders ";
+
+	try {
+	    if (filtro != null)
+		sql += " WHERE " + filtro;
+	    if (limite != null)
+		sql += " LIMIT " + limite;
+	    if (offset != null)
+		sql += " OFFSET " + offset;
+	    sentencia = conexion.createStatement();
+	    ResultSet rs = sentencia.executeQuery(sql);
+	    while (rs.next()) { // Si todavía hay un pedido lo añado al array
+		pedidos.add(new Order(
+			rs.getInt("id"),
+			rs.getString("employee_id"),
+			rs.getString("customer_id"),
+			rs.getString("order_date"),
+			rs.getString("shipped_date"),
+			rs.getString("shipper_id"),
+			rs.getString("ship_name"),
+			rs.getString("ship_address"),
+			rs.getString("ship_city"),
+			rs.getString("ship_state_province"),
+			rs.getString("ship_zip_postal_code"),
+			rs.getString("ship_country_region"),
+			rs.getString("shipping_fee"),
+			rs.getString("taxes"),
+			rs.getString("payment_type"),
+			rs.getString("paid_date"),
+			rs.getString("notes"),
+			rs.getString("tax_rate"),
+			rs.getString("tax_status_id"),
+			rs.getString("status_id")));
+	    };
+	} catch (SQLException e) {
+	    System.err.println("Error en read de Pedidos: " + e.getMessage());
+	    return null;
+	}
+
+	return pedidos;
+    }
+
+}
